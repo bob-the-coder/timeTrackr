53ee272a47dca007bf3635c2ad2d6c2ccd4aca78
Calin Sicoe <calinsicoe@gmail.com>
2017-09-27 22:10:50 +0300

    Add styles for buttons and inputs Add style and script bundles to BundleConfig Remove some template views Move login HTML from Home/Index to Account/Login

--- TimeTrackr/Website/App_Start/BundleConfig.cs
+++ TimeTrackr/Website/App_Start/BundleConfig.cs
@@ -14,6 +14,13 @@ namespace Website
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app")
+                .Include("~/App/enums.js")
+                .Include("~/App/constants.js")
+                .Include("~/App/prototypes.js")
+                .Include("~/App/functions.js")
+                .IncludeDirectory("~/App/models", "*.js", true));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,7 +33,8 @@ namespace Website
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css")
-                      .IncludeDirectory("~/Content/components", "*.css", true));
+                      .IncludeDirectory("~/Content/components", "*.css", true)
+                      .IncludeDirectory("~/Content/pages", "*.css", true));
         }
     }
 }
