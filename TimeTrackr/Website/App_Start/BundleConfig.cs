759ba32300f2c0e19ec91635344636db1883a45c
Calin Sicoe <calinsicoe@gmail.com>
2017-09-27 21:58:36 +0300

    Add some basic stylesheets Remove some template html Add login UI Configure server to use IIS

--- TimeTrackr/Website/App_Start/BundleConfig.cs
+++ TimeTrackr/Website/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Website
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css")
+                      .IncludeDirectory("~/Content/components", "*.css", true));
         }
     }
 }
